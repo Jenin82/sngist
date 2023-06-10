423c10931b96bd55e6462bd55ac903f23820cdfe
Jenin82 <jenin8282@gmail.com>
2023-06-10 14:35:39 +0530

    name picker done

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -7,11 +7,31 @@ interface NameData {
 }
 
 const Home = () => {
-	const data: NameData[]  = [
-        { id: 1, name: "Name", imageLink: 'true' },
-        { id: 2, name: "Name2", imageLink: 'true' },
-        { id: 3, name: "Name3", imageLink: 'true' },
-        { id: 4, name: "Name4", imageLink: 'true' },
+	const data: NameData[] = [
+        {
+            id: 1,
+            name: "Alan K Anil",
+            imageLink:
+                "https://www.photoshopessentials.com/newsite/wp-content/uploads/2018/08/resize-images-print-photoshop-f.jpg",
+        },
+        {
+            id: 2,
+            name: "Name2",
+            imageLink:
+                "https://www.photoshopessentials.com/newsite/wp-content/uploads/2018/08/resize-images-print-photoshop-f.jpg",
+        },
+        {
+            id: 3,
+            name: "Name3",
+            imageLink:
+                "https://www.photoshopessentials.com/newsite/wp-content/uploads/2018/08/resize-images-print-photoshop-f.jpg",
+        },
+        {
+            id: 4,
+            name: "Name4",
+            imageLink:
+                "https://www.photoshopessentials.com/newsite/wp-content/uploads/2018/08/resize-images-print-photoshop-f.jpg",
+        },
     ];
     return (
 		<div>
--- src/modules/RandomNamePicker/RandomNamePicker.tsx
+++ src/modules/RandomNamePicker/RandomNamePicker.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './randomNamePicker.css'
 
 interface NameData {
     id: number;
@@ -8,41 +9,77 @@ interface NameData {
 
 interface RandomNamePickerProps {
     data: NameData[];
+    className?: string;
 }
 
-const RandomNamePicker: React.FC<RandomNamePickerProps> = ({ data }) => {
+const RandomNamePicker: React.FC<RandomNamePickerProps> = ({
+    data,
+}) => {
     const [selectedName, setSelectedName] = useState<string>("");
     const [selectedImage, setSelectedImage] = useState<string>("");
     const [usedNames, setUsedNames] = useState<string[]>([]);
+    const [rolling, setRolling] = useState<boolean>(false);
 
     const handlePickRandomName = () => {
-        const availableNames = data.filter(
-            (nameData) => !usedNames.includes(nameData.name)
-        );
-        if (availableNames.length === 0) {
-            // All names have been used, reset the usedNames array
-            setUsedNames([]);
-            setSelectedName("");
-            setSelectedImage("");
-            return;
-        }
+        if (rolling) return; // Prevent picking a name while rolling animation is active
+        setRolling(true);
 
-        const randomIndex = Math.floor(Math.random() * availableNames.length);
-        const randomData = availableNames[randomIndex];
-        setSelectedName(randomData.name);
-        setSelectedImage(randomData.imageLink);
-        setUsedNames([...usedNames, randomData.name]);
+        // Show rolling animation for 3 seconds
+        setTimeout(() => {
+            setRolling(false);
+
+            const availableNames = data.filter(
+                (nameData) => !usedNames.includes(nameData.name)
+            );
+            if (availableNames.length === 0) {
+                // All names have been used, reset the usedNames array
+                setUsedNames([]);
+                setSelectedName("");
+                setSelectedImage("");
+                return;
+            }
+
+            const randomIndex = Math.floor(
+                Math.random() * availableNames.length
+            );
+            const randomData = availableNames[randomIndex];
+            setSelectedName(randomData.name);
+            setSelectedImage(randomData.imageLink);
+            setUsedNames([...usedNames, randomData.name]);
+        }, 4000);
     };
 
     return (
-        <div>
-            <button onClick={handlePickRandomName}>Pick a Random Name</button>
-            {selectedName && (
-                <div>
-                    <p>Selected Name: {selectedName}</p>
-                    <img src={selectedImage} alt={selectedName} />
+        <div className={"random-name-picker"}>
+            {rolling ? (
+                <div className="scene">
+                    <div className="shadow"></div>
+                    <div className="jumper">
+                        <div className="spinner">
+                            <div className="scaler">
+                                <div className="loader">
+                                    <div className="cuboid">
+                                        <div className="cuboid__side"></div>
+                                        <div className="cuboid__side"></div>
+                                        <div className="cuboid__side"></div>
+                                        <div className="cuboid__side"></div>
+                                        <div className="cuboid__side"></div>
+                                        <div className="cuboid__side"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            ) : (
+                selectedName && (
+                    <div>
+                        <img src={selectedImage} alt={selectedName} />
+                        <h1>{selectedName}</h1>
+                    </div>
+                )
             )}
+            <button onClick={handlePickRandomName}>Pick a Random Name</button>
         </div>
     );
 };
