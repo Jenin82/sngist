a562b602be278f74170e1f0baae5a5e3d99f4184
Jenin82 <jenin8282@gmail.com>
2023-06-10 07:41:52 +0530

    another test

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import NotFound from "./modules/NotFound/NotFound";
 import Home from "./modules/Home/Home";
-
-const router = createBrowserRouter([
-    {
-        path: "*",
-        element: <NotFound />,
-    },
-    {
-        path: "404",
-        element: <NotFound />,
-    },
-    // {
-    //     path: "/",
-    //     element: <AuthRoutes />,
-    //     children: [
-    //         { path: "/", element: <Login /> },
-    //         { path: "register", element: <Onboarding /> },
-    //         { path: "login", element: <Login /> },
-    //         { path: "forgot-password", element: <ForgotPassword /> },
-    //         { path: "reset-password", element: <ResetPassword /> },
-    //     ],
-    // },
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/test",
-        element: <NotFound />,
-    },
-]);
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
 }
+
 
 export default App;
