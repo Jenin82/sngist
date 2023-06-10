1f20a89610df4770b6a5a94b975dc96fffd12944
Jenin82 <jenin8282@gmail.com>
2023-06-10 06:33:46 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import NotFound from "./modules/NotFound/NotFound";
+import Home from "./modules/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+    {
+        path: "404",
+        element: <NotFound />,
+    },
+    // {
+    //     path: "/",
+    //     element: <AuthRoutes />,
+    //     children: [
+    //         { path: "/", element: <Login /> },
+    //         { path: "register", element: <Onboarding /> },
+    //         { path: "login", element: <Login /> },
+    //         { path: "forgot-password", element: <ForgotPassword /> },
+    //         { path: "reset-password", element: <ResetPassword /> },
+    //     ],
+    // },
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
