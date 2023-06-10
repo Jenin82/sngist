9c6c5bbd478e8e62ecccee1721115497daf64236
Jenin82 <jenin8282@gmail.com>
2023-06-10 07:44:52 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="*" element={<NotFound />} />
+                <Route path="test" element={<Home />} />
             </Routes>
         </div>
     );
